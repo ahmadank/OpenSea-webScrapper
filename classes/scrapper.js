c84d596bb8cec48f99bec433a7cd7c5b249e2cb5
ahmadank <ahmedandnq@hotmail.com>
2022-03-13 17:46:11 -0400

    Test

--- classes/scrapper.js
+++ classes/scrapper.js
@@ -8,7 +8,7 @@ const c = new Crawler({
     
     if (error) throw error;
     try{
-    console.log(res.uri)
+    console.log(res.options.uri)
     prices.add([JSON.parse(res.body).stats.floor_price])
     }catch{
       prices.add(["Error"])
