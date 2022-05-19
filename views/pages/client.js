1891febf675951539f1a3ea3b8a2befb28c98e5e
ahmadank <ahmedandnq@hotmail.com>
2022-05-18 23:12:24 -0400

    small fix

--- views/pages/client.js
+++ views/pages/client.js
@@ -1,17 +1,7 @@
-onload();
+var socket = io();
+var el;
 
-// const socket = io('http://localhost:3000')
-//call this function when a website loads
-function onload() {
-  $(document).ready(function () {
-    //every 5 secs every jquery refreshs the data on the webpage
-    setInterval(function () {
-      console.log("Hello");
-      $("#projects").load(window.location.href + " #request");
-    }, 5000);
-  });
-}
-
-// function addProject(){
-//     console.log("This is working")
-//   }
+socket.on('time', function(timeString) {
+  el = document.getElementById('server-time')
+  el.innerHTML = 'Server time: ' + timeString;
+});
